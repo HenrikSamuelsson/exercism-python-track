3333300dd478a1ffbe3c390da71f9468070a5598
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2020-01-25 09:11:42 +0100

    Hangman version that passes all the tests.

--- python/hangman/hangman.py
+++ python/hangman/hangman.py
@@ -1,5 +1,4 @@
 # Game status categories
-# Change the values as you see fit
 STATUS_WIN = "win"
 STATUS_LOSE = "lose"
 STATUS_ONGOING = "ongoing"
@@ -9,36 +8,37 @@ class Hangman:
     def __init__(self, word):
         self.remaining_guesses = 9
         self.status = STATUS_ONGOING
-        self.guessed_letters = set()
+        self.letters_found = list()
         self.word = word
 
     def guess(self, char):
+        """Handling of guesses from the player."""
         if self.status == STATUS_LOSE:
             raise ValueError("No more guesses, you lost")
         elif self.status == STATUS_WIN:
-            raise ValueError("No more guesses, you won")
-
-        if self.status == STATUS_ONGOING:
-            self.guessed_letters.add(char)
-            print(self.get_masked_word())
-            if self.get_masked_word() == self.word:
-                self.status = STATUS_WIN
-            else:
+            raise ValueError("No more guesses, you have already won")
+        elif self.status == STATUS_ONGOING:
+            if char in self.letters_found or char not in self.word:
+                # Handling of an incorrect guess.
                 self.remaining_guesses -= 1
-            print(self.remaining_guesses)
-        if self.remaining_guesses < 0:
-            self.status = STATUS_LOSE
-        print(self.status)
-        print(STATUS_LOSE)
+                if self.remaining_guesses < 0:
+                    self.status = STATUS_LOSE
+            else:
+                # Handling of a correct guess.
+                self.letters_found.append(char)
+                if self.get_masked_word() == self.word:
+                    self.status = STATUS_WIN
 
     def get_masked_word(self):
+        """Get the word to be guessed with not guessed letters being masked."""
         masked_word = ""
         for c in self.word:
-            if c in self.guessed_letters:
+            if c in self.letters_found:
                 masked_word += c
             else:
                 masked_word += "_"
         return masked_word
 
     def get_status(self):
+        """Get the current game status."""
         return self.status
