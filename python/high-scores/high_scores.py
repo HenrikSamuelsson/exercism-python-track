b9380a08e8597764cc66a30e98ece26b65d32c4a
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2020-01-26 16:38:05 +0100

    Remove a debug print.

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,10 +1,18 @@
 def latest(scores):
+    """For retrieving the latest score added to the list."""
     return scores[-1]
 
 
 def personal_best(scores):
-    return max(scores)
+    """For getting the best score in a list of scorers."""
+    return max(scores, default=None)
 
 
 def personal_top_three(scores):
-    pass
+    """Gets the three best scores from a list of scores."""
+    top_three_scores = []
+    for i in range(3):
+        if personal_best(scores):
+            top_three_scores.append(personal_best(scores))
+            scores.remove(top_three_scores[i])
+    return top_three_scores
